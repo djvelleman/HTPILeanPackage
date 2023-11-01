9a7747421648d9272b008524c0d5ce015e6bde83
djvelleman <110697570+djvelleman@users.noreply.github.com>
2023-10-31 20:42:22 -0400

    Update to 4.2.0

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package htpi {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "2df6bb4167844ead7631ba547d6a44d465c0b37b"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.2.0"
 
 @[default_target]
 lean_lib HTPILib {
